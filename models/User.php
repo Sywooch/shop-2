9929452af495a15eefc32f76601699f70e89fb4b
kakoi-to-pirat <kakoi-to.pirat@yandex.ru>
2017-12-03 17:21:04 +0300

    init project and create authorization

---
+++ models/User.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace app\models;
+
+use yii\web\IdentityInterface;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property int $isAdmin
+ * @property string $photo
+ */
+class User extends \yii\db\ActiveRecord implements IdentityInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['isAdmin'], 'integer'],
+            [['name', 'email', 'password', 'photo'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'email' => 'Email',
+            'password' => 'Password',
+            'isAdmin' => 'Is Admin',
+            'photo' => 'Photo',
+        ];
+    }
+
+
+    public static function findIdentity($id)
+    {
+        return User::findOne($id);
+    }
+
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        // TODO: Implement findIdentityByAccessToken() method.
+    }
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    public function getAuthKey()
+    {
+        // TODO: Implement getAuthKey() method.
+    }
+
+
+    public function validateAuthKey($authKey)
+    {
+        // TODO: Implement validateAuthKey() method.
+    }
+
+    public static function findByUsername($username)
+    {
+        return User::find()->where(['name' => $username])->one();
+    }
+
+    public function validatePassword($password)
+    {
+        return ($this->password == $password) ? true : false;
+    }
+
+    public function create()
+    {
+        return $this->save(false);
+    }
+}
---
+++ modules/admin/Module.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\modules\admin;
+
+use Yii;
+use yii\filters\AccessControl;
+
+/**
+ * admin module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+
+    public $controllerNamespace = 'app\modules\admin\controllers';
+    public $layout ='main';
+    public $layoutPath ='@app/views/layouts/admin/layouts';
+
+    /**
+     *
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access'    =>  [
+                'class' =>  AccessControl::className(),
+                'denyCallback'  =>  function($rule, $action)
+                {
+                    throw new \yii\web\NotFoundHttpException();
+                },
+                'rules' =>  [
+                    [
+                        'allow' =>  true,
+                        'matchCallback' =>  function($rule, $action)
+                        {
+                            if (Yii::$app->user->isGuest) {
+                                return false;
+                            }
+                            return Yii::$app->user->identity->isAdmin;
+                        }
+                    ]
+                ]
+            ]
+        ];
+    }
+
+    public function init()
+    {
+        parent::init();
+        // custom initialization code goes here
+    }
+}
